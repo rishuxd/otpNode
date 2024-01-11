63ccd4ad12b2f9688b600cd1747635e628f1d4ce
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2024-01-12 04:18:10 +0530

    setup done

---
+++ src/routes/auth.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const checkAuth = require("../middlewares/checkAuth");
+const checkAdmin = require("../middlewares/checkAdmin");
+const {
+  loginWithPhoneOtp,
+  fetchCurrentUser,
+  createNewUser,
+  verifyPhoneOtp,
+  handleAdmin,
+} = require("../controllers/auth.controller");
+
+router.post("/register", createNewUser);
+
+router.post("/login_with_phone", loginWithPhoneOtp);
+
+router.post("/verify", verifyPhoneOtp);
+
+router.get("/me", checkAuth, fetchCurrentUser);
+
+router.get("/admin", checkAuth, checkAdmin, handleAdmin);
+
+module.exports = router;
